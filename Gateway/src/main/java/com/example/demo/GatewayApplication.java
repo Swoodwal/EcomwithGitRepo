3be23981e0641ed4d706f8450e49addc568bc0a0
Sidak <swoodwal_be20@thapar.edu>
2024-03-17 13:29:57 +0530

    updated files

--- Gateway/src/main/java/com/example/demo/GatewayApplication.java
+++ Gateway/src/main/java/com/example/demo/GatewayApplication.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-
+@CrossOrigin(origins = "*")
 public class GatewayApplication {
 	
 	  @Bean
